00e895869764006d6d1574c67ba0cfd066a0fdb7
Akilah Powell <atpjun@gmail.com>
2022-11-30 04:07:39 -0400

    created first page sign in / sign up in app js

--- app.js
+++ app.js
@@ -2,10 +2,34 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 
-/* Get DOM Elements */
+const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
-/* State */
+const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
 
-/* Events */
+redirectIfLoggedIn();
 
-/* Display Functions */
+signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signUpUser(signUpemail.value, signUpPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
+
+signInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signInUser(signInEmail.value.signInPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    } else {
+        console.error(user);
+    }
+});
